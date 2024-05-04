2f719fa30d3d4234bbce0420c46b143dc4129397
Mohammed Rabil <rabil@techie.com>
2024-05-04 22:29:42 +0530

    chore: Add MongoDB connection URL to environment variables

--- groq_chat/bot.py
+++ groq_chat/bot.py
@@ -5,7 +5,7 @@ from telegram.ext import (
     CommandHandler,
     MessageHandler,
     CallbackQueryHandler,
-    ConversationHandler
+    ConversationHandler,
 )
 from groq_chat.handlers import (
     start,
@@ -19,10 +19,11 @@ from groq_chat.handlers import (
     start_system_prompt,
     get_system_prompt,
     cancelled_system_prompt,
-    info_command_handler
+    info_command_handler,
 )
 from groq_chat.filters import AuthFilter, MessageFilter
 from dotenv import load_dotenv
+from mongopersistence import MongoPersistence
 import logging
 
 load_dotenv()
@@ -36,29 +37,55 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+persistence = MongoPersistence(
+    mongo_url=os.getenv("MONGODB_URL"),
+    db_name="groq-chatbot",
+    name_col_user_data="user_data",
+    name_col_bot_data="bot_data",
+    name_col_chat_data="chat_data",
+    name_col_conversations_data="conversations_data",
+    create_col_if_not_exist=True,  # optional
+    ignore_general_data=["cache"],
+)
+
 
 def start_bot():
     logger.info("Starting bot")
-    app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
-    app.add_error_handler(lambda _, __: logger.error("Exception while handling an update:", exc_info=True))
+    app = (
+        Application.builder()
+        .token(os.getenv("BOT_TOKEN"))
+        .persistence(persistence)
+        .build()
+    )
+    app.add_error_handler(
+        lambda _, __: logger.error("Exception while handling an update:", exc_info=True)
+    )
 
     app.add_handler(CommandHandler("start", start, filters=AuthFilter))
     app.add_handler(CommandHandler("help", help_command, filters=AuthFilter))
     app.add_handler(CommandHandler("new", new_command_handler, filters=AuthFilter))
     app.add_handler(CommandHandler("model", model_command_handler, filters=AuthFilter))
     app.add_handler(CommandHandler("info", info_command_handler, filters=AuthFilter))
-    
+
     app.add_handler(
         ConversationHandler(
-            entry_points=[CommandHandler("system_prompt", start_system_prompt, filters=AuthFilter)],
+            entry_points=[
+                CommandHandler("system_prompt", start_system_prompt, filters=AuthFilter)
+            ],
             states={
                 SYSTEM_PROMPT_SP: [MessageHandler(MessageFilter, get_system_prompt)],
-                CANCEL_SP: [CommandHandler("cancel", cancelled_system_prompt, filters=AuthFilter)],
+                CANCEL_SP: [
+                    CommandHandler(
+                        "cancel", cancelled_system_prompt, filters=AuthFilter
+                    )
+                ],
             },
-            fallbacks=[CommandHandler("cancel", cancelled_system_prompt, filters=AuthFilter)],
+            fallbacks=[
+                CommandHandler("cancel", cancelled_system_prompt, filters=AuthFilter)
+            ],
         )
     )
-    
+
     app.add_handler(MessageHandler(MessageFilter, message_handler))
     app.add_handler(
         CallbackQueryHandler(change_model_callback_handler, pattern="^change_model_")
